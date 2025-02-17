8e58acf82922d809a491fd42801b9fd4618699cc
Sebastian <sebastianlakla@gmail.com>
2025-02-17 19:09:24 +0100

    Create main div for slider container with styles

--- src/components/slider-component/slider-container.vue
+++ src/components/slider-component/slider-container.vue
@@ -2,12 +2,17 @@
 import sliderItems from './slider-items.vue'
 </script>
 <template>
-  <sliderItems />
+    <div class="slider-container">
+        <sliderItems />
+    </div>
 </template>
 <style lang="scss">
 $main-bg-clr: rgb(7, 140, 248);
 body{
+    .slider-container{
     background-color: $main-bg-clr;
     height: 100svh;
+    padding: .5em;
+    }
 }
 </style>
--- src/components/slider-component/slider-items.vue
+++ src/components/slider-component/slider-items.vue
@@ -8,6 +8,5 @@
 $main-font-clr: rgb(0, 0, 0);
 .description{
     color: $main-font-clr;
-    padding: 1em 0;
 }
 </style>
