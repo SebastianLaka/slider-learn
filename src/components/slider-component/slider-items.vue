697dcee40854382a60c7ac943ea0efdb1817817d
Sebastian <sebastianlakla@gmail.com>
2025-02-17 19:48:52 +0100

    Migrate img direcory to components

--- src/components/slider-component/slider-items.vue
+++ src/components/slider-component/slider-items.vue
@@ -1,8 +1,5 @@
 <template>
-    <p class="description">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus consequuntur iusto, provident quod cum facilis impedit minima nulla beatae nobis!
-        lorem100
-    </p>
+
 </template>
 <style lang="scss">
 $main-font-clr: rgb(0, 0, 0);
