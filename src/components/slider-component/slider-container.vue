b3ecef80bcfc7d631861a3d9f0e2c27237d977f4
Sebastian <sebastianlakla@gmail.com>
2025-02-19 20:21:30 +0100

    Change structure of files. Change way of doing slider for easie

--- src/components/slider-component/slider-container.vue
+++ src/components/slider-component/slider-container.vue
@@ -1,30 +1,39 @@
 <template>
   <div class="slider-container">
-    <img class="slider-container__image" src="../img/dodge-viper.jpg" alt="Dodge Viper SRT-10" />
-    <sliderButtons/>
+    <div class="img-area">
+      <sliderButtons />
+    </div>
   </div>
 </template>
 <style lang="scss">
 $main-bg-clr: rgb(7, 140, 248);
+
 body {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100svh;
   background-color: $main-bg-clr;
   padding: 1em;
   .slider-container {
-    position: relative;
     display: flex;
-    max-height: 35em;
-   
-    &__image {
-      object-fit: contain;
-      max-height: 35em;
+    align-items: center;
+    justify-content: center;
+    min-height: 95svh;
+    max-width: 1000px;
+    margin: 0 auto;
+    position: relative;
+    z-index: 0;
+    .img-area {
+      position: absolute;
+      background-image: url(../img/porsche-911-dakkar.jpg);
+      background-size: cover;
+      background-position: center center;
+      background-repeat: no-repeat;
+      margin: 0 auto;
+      width: 100%;
+      min-height: 75%;
+      z-index: 1;
     }
   }
 }
 </style>
 <script setup>
-import sliderButtons from "./slider-controls.vue";
+import sliderButtons from './slider-controls.vue'
 </script>
--- src/components/slider-component/slider-controls.vue
+++ src/components/slider-component/slider-controls.vue
@@ -1,21 +1,22 @@
 <template>
     <div class="buttons-area">
-        <button class="buttons-area__left"><</button>
-        <button class="buttons-area__right">></button>
+        <button class="buttons-area__left"><img src="../icons/arrow-left.svg" alt="slider arrow left"></button>
+        <button class="buttons-area__right"><img src="../icons/arrow-right.svg" alt="slider arrow right"></button>
     </div>
 </template>
 <style scoped lang="scss">
 .buttons-area{
-    position: absolute;
-    width: 100%;
     display: flex;
+    position: absolute;
     justify-content: space-between;
     top: 50%;
+    width: 100%;
+    height: calc(100% + .3%);
     transform: translateY(-50%);
     &__left, &__right{
         padding: 1em 1.5em;
         border: none;
-        // background-color: transparent;
+        background-color: rgba(0,0,0, .5);
         cursor: pointer;
     }
 }
